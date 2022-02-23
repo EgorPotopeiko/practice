042d475b631bc060bbfa2f4d900833ab722e8ad0
Egor Potopeiko <egor@example.com>
2022-02-23 13:52:49 +0300

    Created types for header

--- src/components/header/no-auth/Header.tsx
+++ src/components/header/no-auth/Header.tsx
@@ -1,17 +1,39 @@
-import { Button, Input, PageHeader, Typography } from 'antd';
+import { Button, Checkbox, Input, PageHeader, Select, Slider, Switch, Typography } from 'antd';
 import React from 'react';
 import './Header.less';
 
-const { Title } = Typography;
+const { Title, Text } = Typography;
+
+const { Option } = Select;
+
 
 const Header: React.FC = () => {
     return (
         <div className="header">
             <PageHeader>
-                <Title>Shop</Title>
-                <div className='header__user'>
-                    <Input placeholder="input search text" />
-                    <Button>Войти</Button>
+                <div className='header__wrap'>
+                    <Title>Shop</Title>
+                    <div className='header__user'>
+                        <Input placeholder="input search text" />
+                        <Button>Войти</Button>
+                    </div>
+                </div>
+                <div className='header__filters'>
+                    <>
+                        <Select placeholder="Производитель">
+                            <Option value="Horns and hooves"><Checkbox>Рога и копыта</Checkbox></Option>
+                            <Option value="ZooParadise"><Checkbox>ZooParadise</Checkbox></Option>
+                            <Option value="Purina"><Checkbox>Purina</Checkbox></Option>
+                        </Select>
+                    </>
+                    <>
+                        <Text>В наличии</Text>
+                        <Switch defaultChecked />
+                    </>
+                    <>
+                        <Text>Цена</Text>
+                        <Slider range max={20} defaultValue={[0, 20]} />
+                    </>
                 </div>
             </PageHeader>
         </div>
