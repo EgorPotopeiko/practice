70c52d2d4b35ace7ddc5c5dc4a9202fa81e1e2c4
Egor Potopeiko <egor@example.com>
2022-02-28 17:54:44 +0300

    Added loader in project

---
+++ src/components/loader/loader.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Loader: React.FC = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-ej79qdf0wk"><div className="ldio-ew0pw6iqq0k">
+            <div></div>
+            <div></div>
+            <div><div></div></div>
+            <div><div></div></div>
+        </div></div>
+    )
+
+}
+export default Loader
