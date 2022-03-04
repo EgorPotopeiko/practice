fabb198b5cb7d63c04524efa050dbe665b0b6509
Egor Potopeiko <egor@example.com>
2022-03-04 15:37:23 +0300

    Added order modal

--- src/pages/shoppingCart/cart/Cart.tsx
+++ src/pages/shoppingCart/cart/Cart.tsx
@@ -1,14 +1,17 @@
 /* eslint-disable array-callback-return */
-import React from 'react';
+import { Button } from 'antd';
+import React, { useState } from 'react';
 import "./Cart.less"
 import CartItems from './cartItems/cartItems';
 import CartOrder from './cartOrder/cartOrder';
 
 const Cart: React.FC = () => {
+    const [visible, setVisible] = useState(false);
     return (
         <div className="cart">
             <CartItems />
-            <CartOrder />
+            <Button onClick={() => setVisible(true)}>1</Button>
+            <CartOrder visible={visible} setVisible={setVisible} />
         </div>
     );
 }
--- src/pages/shoppingCart/cart/cartOrder/cartOrder.tsx
+++ src/pages/shoppingCart/cart/cartOrder/cartOrder.tsx
@@ -1,26 +1,123 @@
-import { Divider, Radio, Typography } from 'antd';
+import { Col, Drawer, Form, Input, Row, Typography } from 'antd';
 import React from 'react';
+import MaskedInput from 'antd-mask-input';
 import "./cartOrder.less"
+import { SubmitButton } from 'formik-antd';
+import { Formik } from 'formik';
 
-const { Title, Text } = Typography
+const { Title, Text } = Typography;
 
-const CartOrder: React.FC = () => {
+interface Props {
+    visible: any,
+    setVisible: any
+}
+
+const CartOrder: React.FC<Props> = ({ visible, setVisible }) => {
+
+    const onClose = () => {
+        setVisible(false)
+    };
+
     return (
-        <div className="cart__order">
-            <div className='order__header'>
-                <Title level={4}>Выбрано 0 товаров</Title>
-                <Text>0 руб</Text>
-            </div>
-            <Divider />
-            <div className='order__delivery'>
-                <Text strong>Способ доставки</Text>
-                <Radio.Group name="delivery" defaultValue="self">
-                    <Radio value="self">Самовывоз</Radio>
-                    <Radio value="post">Почтой</Radio>
-                    <Radio value="deliver">Курьером</Radio>
-                </Radio.Group>
-            </div>
-        </div>
+        <Drawer
+            title="Create a order"
+            width={600}
+            onClose={onClose}
+            visible={visible}
+            bodyStyle={{ paddingBottom: 80 }}
+        >
+            <Formik initialValues={{ town: '', street: '', house: '', flat: '', entrance: '', level: '', number: '', name: '', comment: '' }} validateOnBlur onSubmit={(values) => console.log(values)}>
+                {() => (
+                    <Form layout="vertical" hideRequiredMark>
+                        <Row gutter={16}>
+                            <Col span={18}>
+                                <Form.Item>
+                                    <Title level={4}>Выбрано 0 товаров</Title>
+                                </Form.Item>
+                            </Col>
+                            <Col span={6}>
+                                <Form.Item>
+                                    <Text>0 руб</Text>
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                        <Form.Item
+                            name="town"
+                            label="Город"
+                            rules={[{ required: true, message: 'Введите город' }]}
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item
+                            name="street"
+                            label="Улица"
+                            rules={[{ required: true, message: 'Введите улицу' }]}
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Row gutter={16}>
+                            <Col span={6}>
+                                <Form.Item
+                                    name="house"
+                                    label="Дом"
+                                    rules={[{ required: true, message: 'Введите номер дома' }]}
+                                >
+                                    <Input type='number' />
+                                </Form.Item>
+                            </Col>
+                            <Col span={6}>
+                                <Form.Item
+                                    name="flat"
+                                    label="Квартира"
+                                    rules={[{ required: true, message: 'Введите номер квартиры' }]}
+                                >
+                                    <Input type='number' />
+                                </Form.Item>
+                            </Col>
+                            <Col span={6}>
+                                <Form.Item
+                                    name="entrance"
+                                    label="Подъезд"
+                                >
+                                    <Input type='number' />
+                                </Form.Item>
+                            </Col>
+                            <Col span={6}>
+                                <Form.Item
+                                    name="level"
+                                    label="Этаж"
+                                >
+                                    <Input type='number' />
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                        <Form.Item
+                            name="number"
+                            label="Телефон"
+                            rules={[{ required: true, message: 'Введите свой номер телефона' }]}
+                        >
+                            <MaskedInput mask="+375 (11)-111-11-11" name="telephone" placeholder="+375 (__)-___-__-__" />
+                        </Form.Item>
+                        <Form.Item
+                            name="name"
+                            label="ФИО"
+                            rules={[{ required: true, message: 'Введите ФИО' }]}
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item
+                            name="comment"
+                            label="Комментарий к заказу">
+                            <Input.TextArea rows={4} />
+                        </Form.Item>
+                        <Form.Item
+                            name="submit">
+                            <SubmitButton>Оформить заказ</SubmitButton>
+                        </Form.Item>
+                    </Form>
+                )}
+            </Formik>
+        </Drawer>
     );
 }
 
