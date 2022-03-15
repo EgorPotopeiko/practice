97a70416b446580c5d73b4248845cd92bfc817af
Egor Potopeiko <egor@example.com>
2022-03-15 12:34:47 +0300

    Added import JSON-files from project

--- src/pages/adminPanel/adminProducts/filters/productsFilters.tsx
+++ src/pages/adminPanel/adminProducts/filters/productsFilters.tsx
@@ -1,13 +1,15 @@
-import { Button, Input, Select } from 'antd';
+/* eslint-disable array-callback-return */
+import { Button, Input, Select, Upload } from 'antd';
 import { Form, SubmitButton, Input as FormInput } from 'formik-antd';
 import { Field, Formik } from 'formik';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './productsFilters.less';
 import Modal from 'antd/lib/modal/Modal';
 import { customAlphabet } from 'nanoid';
 import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
 import { selectValues } from '../../../../components/header/Header'
-import { addProduct } from '../../../../store/products/actions';
+import { addProduct, setProducts } from '../../../../store/products/actions';
+import axios from 'axios';
 
 interface Props {
     setSearchName: React.Dispatch<React.SetStateAction<string>>,
@@ -25,7 +27,11 @@ const validate = (value: any) => {
 }
 
 const ProductsFilter: React.FC<Props> = ({ setSearchName, setSearchArticle, setSearchCategory, setSearchStatus }) => {
+    const products = useSelector((state: RootStateOrAny) => state.productsReducer.products);
     const [visible, setVisible] = useState(false);
+    const [fileList, setFileList]: any = useState([]);
+    const formData = new FormData();
+    const [upLoading, setUpLoading] = useState(false);
     const dispatch = useDispatch()
     const categoryValues = useSelector((state: RootStateOrAny) => state.filterReducer.listCategories);
     const filterCategories = categoryValues.filter((item: any) => item !== 'all')
@@ -38,12 +44,51 @@ const ProductsFilter: React.FC<Props> = ({ setSearchName, setSearchArticle, setS
             const nanoid_4 = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', 4)
             const nanoid_12 = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', 12)
             values.id = `${nanoid_8()}-${nanoid_4()}-${nanoid_4()}-${nanoid_4()}-${nanoid_12()}`
-            values.date = new Date().toLocaleDateString()
+            values.date = new Date().toLocaleString()
             values.available = values.available === 'true' ? true : false
             dispatch(addProduct(values))
             setVisible(false)
         }, 3000)
     }
+    const handleUpload = async () => {
+        fileList.forEach((file: any) => {
+            formData.append('files[]', file);
+        });
+        setUpLoading(true)
+        fetch('https://www.mocky.io/v2/5cc8019d300000980a055e76', {
+            method: 'POST',
+            body: formData,
+        })
+            .then(res => res.json())
+            .then(() => {
+                setFileList([])
+                console.log('upload successfully.');
+            })
+            .catch(() => {
+                console.log('upload failed.');
+            })
+            .finally(() => {
+                setUpLoading(false)
+            });
+    }
+    const props = {
+        onRemove: (file: any) => {
+            const index = fileList.indexOf(file);
+            const newFileList = fileList.slice();
+            newFileList.splice(index, 1);
+            setFileList(newFileList)
+        },
+        beforeUpload: async (file: any) => {
+            const res = await axios.get(`http://localhost:3000/db/${file.name}`);
+            dispatch(setProducts(res.data))
+            setFileList([...fileList, file]);
+            return false;
+        },
+        fileList,
+    };
+    useEffect(() => {
+        localStorage.setItem("products", JSON.stringify(products))
+    }, [products])
     return (
         <div className="admin__filters">
             <div className='admin__filters-block'>
@@ -61,7 +106,9 @@ const ProductsFilter: React.FC<Props> = ({ setSearchName, setSearchArticle, setS
             </div>
             <div className='admin__filters-btns'>
                 <Button type='default' onClick={() => setVisible(true)}>Добавить новый товар</Button>
-                <Button type='primary'>Импорт</Button>
+                <Upload {...props}>
+                    <Button type='primary' onClick={handleUpload} loading={upLoading}>Импорт</Button>
+                </Upload>
             </div>
             <Modal title="Создание нового товара" visible={visible} onCancel={onCancel} footer={null} width={600}>
                 <div className='admin__createModal'>
