da18a9b4e463ee6641f0c5a85c8f89fee7e0f465
Egor Potopeiko <egor@example.com>
2022-03-22 10:42:54 +0300

    Corrections on styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.less";
 import { useRoutes } from './routing/routes';
 
 function App() {
-    const routes = useRoutes(true, "admin");
+    const routes = useRoutes(false, "guest");
     return (
         <div className="App">
             {routes}
