1c32b1b461449d4b721a8ab9d18d3806912f153a
Egor Potopeiko <egor@example.com>
2022-04-04 16:45:19 +0300

    Added styles for user components

--- src/pages/shoppingCart/cart/cartOrder/СartOrder.tsx
+++ src/pages/shoppingCart/cart/cartOrder/СartOrder.tsx
@@ -141,208 +141,214 @@ const CartOrder: React.FC<Props> = ({ visible, setVisible }) => {
         <>
             <Drawer
                 title="Заявка на заказ"
-                width={610}
+                width={950}
                 onClose={onClose}
                 visible={visible}
                 bodyStyle={{ paddingBottom: 80 }}
             >
-                <Form onFinish={handleSubmit}>
-                    <Row gutter={16}>
-                        <Col span={18}>
-                            <Form.Item>
-                                <Title level={4}>Выбрано {length} товара(-ов)</Title>
-                            </Form.Item>
-                        </Col>
-                        <Col span={6}>
-                            <Form.Item>
-                                <Text>{total} руб</Text>
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                    <Divider />
-                    <Text>Способ доставки</Text>
-                    <Radio.Group defaultValue="курьером" onChange={onChange} value={delivery}>
-                        <Radio value="курьером">Курьером</Radio>
-                        <Radio value="почтой">Почтой</Radio>
-                        <Radio value="самовывоз">Самовывоз</Radio>
-                    </Radio.Group>
-                    <Divider />
-                    <Form.Item
-                        name="town"
-                        hasFeedback
-                        initialValue={values.town}
-                        rules={[{ required: true, message: 'Введите город' }]}
-                    >
-                        <Input
+                <div className='create__modal'>
+                    <Form onFinish={handleSubmit}>
+                        <Row gutter={16}>
+                            <Col span={18}>
+                                <Form.Item>
+                                    <Title level={3}>Выбрано {length} товара(-ов)</Title>
+                                </Form.Item>
+                            </Col>
+                            <Col span={6}>
+                                <Form.Item>
+                                    <Text>{total} руб</Text>
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                        <Text>Способ доставки</Text>
+                        <Radio.Group defaultValue="курьером" onChange={onChange} value={delivery}>
+                            <Radio value="курьером">Курьером</Radio>
+                            <Radio value="почтой">Почтой</Radio>
+                            <Radio value="самовывоз">Самовывоз</Radio>
+                        </Radio.Group>
+                        <Divider />
+                        <Form.Item
                             name="town"
-                            placeholder="Город"
-                            value={values.town}
-                            onChange={handleChange}
-                        />
-                    </Form.Item>
-                    <Form.Item
-                        name="street"
-                        hasFeedback
-                        initialValue={values.street}
-                        rules={[{ required: true, message: 'Введите улицу' }]}
-                    >
-                        <Input
+                            hasFeedback
+                            initialValue={values.town}
+                            rules={[{ required: true, message: 'Введите город' }]}
+                        >
+                            <Input
+                                name="town"
+                                placeholder="Город"
+                                value={values.town}
+                                onChange={handleChange}
+                            />
+                        </Form.Item>
+                        <Form.Item
                             name="street"
-                            placeholder="Улица"
-                            value={values.street}
-                            onChange={handleChange}
-                        />
-                    </Form.Item>
-                    <Row gutter={0} style={{ marginBottom: "24px" }}>
-                        <Col span={5}>
-                            <Form.Item
-                                name="house"
-                                hasFeedback
-                                initialValue={values.house}
-                                rules={[{ required: true, message: 'Введите номер дома' }]}
-                            >
-                                <Input
+                            hasFeedback
+                            initialValue={values.street}
+                            rules={[{ required: true, message: 'Введите улицу' }]}
+                        >
+                            <Input
+                                name="street"
+                                placeholder="Улица"
+                                value={values.street}
+                                onChange={handleChange}
+                            />
+                        </Form.Item>
+                        <Row gutter={0}>
+                            <Col span={5}>
+                                <Form.Item
                                     name="house"
-                                    type="number"
-                                    placeholder="Дом"
-                                    value={values.house}
-                                    onChange={handleChange}
-                                />
-                            </Form.Item>
-                        </Col>
-                        <Col span={5}>
-                            <Form.Item
-                                name="flat"
-                                hasFeedback
-                                initialValue={values.flat}
-                                rules={[{ required: true, message: 'Введите номер квартиры' }]}
-                            >
-                                <Input
+                                    hasFeedback
+                                    initialValue={values.house}
+                                    rules={[{ required: true, message: 'Введите номер дома' }]}
+                                >
+                                    <Input
+                                        name="house"
+                                        type="number"
+                                        placeholder="Дом"
+                                        value={values.house}
+                                        onChange={handleChange}
+                                    />
+                                </Form.Item>
+                            </Col>
+                            <Col span={5}>
+                                <Form.Item
                                     name="flat"
-                                    type="number"
-                                    placeholder="Квартира"
-                                    value={values.flat}
-                                    onChange={handleChange}
-                                />
-                            </Form.Item>
-                        </Col>
-                        <Col span={5}>
-                            <Form.Item
-                                name="entrance"
-                                hasFeedback
-                                initialValue={values.entrance}
-                            >
-                                <Input
+                                    hasFeedback
+                                    initialValue={values.flat}
+                                    rules={[{ required: true, message: 'Введите номер квартиры' }]}
+                                >
+                                    <Input
+                                        name="flat"
+                                        type="number"
+                                        placeholder="Квартира"
+                                        value={values.flat}
+                                        onChange={handleChange}
+                                    />
+                                </Form.Item>
+                            </Col>
+                            <Col span={5}>
+                                <Form.Item
                                     name="entrance"
-                                    type="number"
-                                    placeholder="Подъезд"
-                                    value={values.entrance}
-                                    onChange={handleChange}
-                                />
-                            </Form.Item>
-                        </Col>
-                        <Col span={5}>
-                            <Form.Item
-                                name="level"
-                                hasFeedback
-                                initialValue={values.level}
-                            >
-                                <Input
+                                    hasFeedback
+                                    initialValue={values.entrance}
+                                >
+                                    <Input
+                                        name="entrance"
+                                        type="number"
+                                        placeholder="Подъезд"
+                                        value={values.entrance}
+                                        onChange={handleChange}
+                                    />
+                                </Form.Item>
+                            </Col>
+                            <Col span={5}>
+                                <Form.Item
                                     name="level"
-                                    type="number"
-                                    placeholder="Этаж"
-                                    value={values.level}
-                                    onChange={handleChange}
-                                />
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                    <Form.Item
-                        name="number"
-                        hasFeedback
-                        initialValue={values.number}
-                        rules={[{ required: true, message: 'Введите номер телефона' }]}
-                    >
-                        <MaskedInput mask="+375 (11)-111-11-11" name="number" placeholder="+375 (__)-___-__-__" value={values.number} onChange={handleChange} />
-                    </Form.Item>
-                    <Form.Item
-                        name="name"
-                        hasFeedback
-                        initialValue={values.name}
-                        rules={[{ required: true, message: 'Введите ФИО' }]}
-                    >
-                        <Input
+                                    hasFeedback
+                                    initialValue={values.level}
+                                >
+                                    <Input
+                                        name="level"
+                                        type="number"
+                                        placeholder="Этаж"
+                                        value={values.level}
+                                        onChange={handleChange}
+                                    />
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                        <Form.Item
+                            name="number"
+                            hasFeedback
+                            initialValue={values.number}
+                            rules={[{ required: true, message: 'Введите номер телефона' }]}
+                        >
+                            <MaskedInput mask="+375 (11)-111-11-11" name="number" placeholder="+375 (__)-___-__-__" value={values.number} onChange={handleChange} />
+                        </Form.Item>
+                        <Form.Item
                             name="name"
-                            placeholder="ФИО"
-                            value={values.name}
-                            onChange={handleChange}
-                        />
-                    </Form.Item>
-                    <Form.Item
-                        name="comment"
-                        hasFeedback
-                        initialValue={values.comment}
-                    >
-                        <Input.TextArea
+                            hasFeedback
+                            initialValue={values.name}
+                            rules={[{ required: true, message: 'Введите ФИО' }]}
+                        >
+                            <Input
+                                name="name"
+                                placeholder="ФИО"
+                                value={values.name}
+                                onChange={handleChange}
+                            />
+                        </Form.Item>
+                        <Form.Item
                             name="comment"
-                            placeholder="Комментарий к заказу"
-                            value={values.comment}
-                            onChange={handleChange}
-                        />
-                    </Form.Item>
-                    <Button
-                        type="primary"
-                        htmlType="submit"
-                        disabled={isSubmitting}
-                        loading={loading}
-                    >Оформить заказ</Button>
-                </Form>
+                            hasFeedback
+                            initialValue={values.comment}
+                        >
+                            <Input.TextArea
+                                name="comment"
+                                placeholder="Комментарий к заказу"
+                                value={values.comment}
+                                onChange={handleChange}
+                            />
+                        </Form.Item>
+                        <Button
+                            type="primary"
+                            htmlType="submit"
+                            disabled={isSubmitting}
+                            loading={loading}
+                        >Оформить заказ</Button>
+                    </Form>
+                </div>
             </Drawer>
             <Drawer
                 title="Заказ"
-                width={610}
+                width={950}
                 visible={orderVisible}
                 closable={false}
                 bodyStyle={{ paddingBottom: 80 }}>
-                <Form>
-                    <Row gutter={16}>
-                        <Col span={18}>
-                            <Form.Item>
-                                <Title level={4}>Заказ №{values.id}</Title>
-                            </Form.Item>
-                        </Col>
-                        <Col span={6}>
-                            <Form.Item>
-                                <Text>{values.payment} руб</Text>
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                    <Divider />
-                    <Form.Item>
-                        <Text><b>Способ доставки:</b> {values.delivery}</Text>
-                    </Form.Item>
-                    <Form.Item>
-                        <Text><b>По адресу:</b> {values.town}, {values.street}, {values.house}</Text>
-                    </Form.Item>
-                    <Form.Item>
-                        <Text><b>Получатель:</b> {values.name}</Text>
-                    </Form.Item>
-                    <Form.Item>
-                        <Text>{values.number}</Text>
-                    </Form.Item>
-                    <Button type='primary' loading={loading} onClick={() => finishOrder()}>Оплатить</Button>
-                    <Button type='link' onClick={() => backToOrder()}>Вернуться к заказу</Button>
-                </Form>
+                <div className='order__modal'>
+                    <Form>
+                        <Row gutter={16}>
+                            <Col span={18}>
+                                <Form.Item>
+                                    <Title level={4}>Заказ №{values.id}</Title>
+                                </Form.Item>
+                            </Col>
+                            <Col span={6}>
+                                <Form.Item>
+                                    <Text>{values.payment} руб</Text>
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                        <Divider />
+                        <Form.Item>
+                            <Text><b>Способ доставки:</b> {values.delivery}</Text>
+                        </Form.Item>
+                        <Form.Item>
+                            <Text><b>По адресу:</b> {values.town}, {values.street}, {values.house}</Text>
+                        </Form.Item>
+                        <Form.Item>
+                            <Text><b>Получатель:</b> {values.name}</Text>
+                        </Form.Item>
+                        <Form.Item>
+                            <Text>{values.number}</Text>
+                        </Form.Item>
+                        <Button type='primary' loading={loading} onClick={() => finishOrder()}>Оплатить</Button>
+                        <Button type='link' onClick={() => backToOrder()}>Вернуться к заказу</Button>
+                    </Form>
+                </div>
+
             </Drawer>
             <Modal title="Ваш заказ успешно создан" onCancel={backToMain} visible={successVisible} footer={null}>
-                <Form>
-                    <Form.Item>
-                        <Text>Номер вашего заказа: <b><i>{values.id}</i></b></Text>
-                    </Form.Item>
-                </Form>
+                <div className='success__modal'>
+                    <Form>
+                        <Form.Item>
+                            <Text>Номер вашего заказа: <b><i>{values.id}</i></b></Text>
+                        </Form.Item>
+                    </Form>
+                </div>
+
             </Modal>
         </>
-
     );
 }
 
