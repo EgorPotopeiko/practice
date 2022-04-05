c172f911cabfc1c35e316ccc8bd02b1a9ffc1262
Egor Potopeiko <egor@example.com>
2022-04-05 13:34:16 +0300

    Added loader with correct names

---
+++ src/components/loader/loader.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './loader.less';
+
+const Loader: React.FC = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-m7egy69nk7a"><div className="ldio-wu9t9mnz7i">
+            <div></div>
+            <div></div>
+            <div><div></div></div>
+            <div><div></div></div>
+        </div></div>
+    )
+
+}
+export default Loader
