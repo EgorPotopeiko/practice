40bd9a8d1879372d484a95414184126a61d1071b
Egor Potopeiko <egor@example.com>
2022-04-05 15:10:28 +0300

    Added app with correct name

---
+++ src/app.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import './app.less';
+import { useRoutes } from './routing/routes';
+import { selectUserMenu } from './store/filters/selectors';
+import { selectUser } from './store/login/selectors';
+
+function App() {
+    const user = useSelector(selectUser);
+    const routes = useRoutes(user.isAuth, user.role);
+    const listCategories = useSelector(selectUserMenu);
+    useEffect(() => {
+        localStorage.setItem("categories", JSON.stringify(listCategories))
+    }, [listCategories]);
+    useEffect(() => {
+        localStorage.setItem("user", JSON.stringify(user))
+    }, [user]);
+    return (
+        <div className="App">
+            {routes}
+        </div>
+    );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { ConnectedRouter } from 'connected-react-router';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './App';
+import App from './app';
 import configureStore from './configureStore';
 import history from './history';
 
