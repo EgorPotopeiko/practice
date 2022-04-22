a70899e8fc1902da2c5f9dffd9d463aaa321d673
Egor Potopeiko <egor@example.com>
2022-04-22 16:48:06 +0300

    Added initialValues for all projects

--- src/pages/admin_panel/admin_forms/admin_forms.tsx
+++ src/pages/admin_panel/admin_forms/admin_forms.tsx
@@ -6,26 +6,59 @@ import { Tabs } from 'antd';
 import AdminCalendar from './admin_calendar/admin_calendar';
 import AdminInternet from './admin_internet/admin_internet';
 import AdminCoordinators from './admin_coordinators/admin_coordinators';
+import { Formik } from 'formik';
+import { SubmitButton, Form } from 'formik-antd';
 
 const { TabPane } = Tabs;
 
 const AdminForms: React.FC = () => {
     return (
         <div className='admin__forms'>
-            <Tabs tabPosition='left'>
-                <TabPane key='projects' tab={<AppstoreAddOutlined />}>
-                    <AdminProjects />
-                </TabPane>
-                <TabPane key='calendar' tab={<CalendarOutlined />}>
-                    <AdminCalendar />
-                </TabPane>
-                <TabPane key='internet' tab={<GlobalOutlined />}>
-                    <AdminInternet />
-                </TabPane>
-                <TabPane key='coordinator' tab={<UserOutlined />}>
-                    <AdminCoordinators />
-                </TabPane>
-            </Tabs>
+            <Formik initialValues={{
+                name: '',
+                requestNumber: '',
+                contractNumber: '',
+                contractDate: '',
+                advancePayment: false,
+                igk: '',
+                budget: 0,
+                sessionId: '',
+                goal: '',
+                description: '',
+                completionDate: '',
+                statusId: '',
+                contentThematicIds: [],
+                contentDirectionId: '',
+                contentFormats: [],
+                channels: [],
+                kpis: [],
+                ownerId: '',
+                producerIds: [],
+                coordinatorIds: [],
+                fileIds: [],
+                imageId: ''
+            }}
+                onSubmit={(values) => console.log(values)}>
+                {({ values }) => (
+                    <Form>
+                        <Tabs tabPosition='left'>
+                            <TabPane key='projects' tab={<AppstoreAddOutlined />}>
+                                <AdminProjects />
+                            </TabPane>
+                            <TabPane key='calendar' tab={<CalendarOutlined />}>
+                                <AdminCalendar />
+                            </TabPane>
+                            <TabPane key='internet' tab={<GlobalOutlined />}>
+                                <AdminInternet />
+                            </TabPane>
+                            <TabPane key='coordinator' tab={<UserOutlined />}>
+                                <AdminCoordinators />
+                            </TabPane>
+                        </Tabs>
+                        <SubmitButton>Submit</SubmitButton>
+                    </Form>
+                )}
+            </Formik>
         </div >
     );
 }
