25f6f341223f66ba156f94b7bb3254a27cbfae0b
Egor Potopeiko <egor@example.com>
2022-05-11 11:20:01 +0300

    Added img for error_indicator

--- src/components/error_indicator/error_indicator.tsx
+++ src/components/error_indicator/error_indicator.tsx
@@ -11,8 +11,7 @@ const ErrorIndicator: FC = () => {
             <Card title='Упс!'>
                 <Title level={2}>Произошла ошибка! Попробуйте перезагрузить страницу!</Title>
                 <div className='error__img-block'>
-                    {/*//todo взять из public папки*/}
-                    <Image width={200} src='http://ezotera.ariom.ru/uploads/posts/2012-03/1332866659_oshibka-300x300.png' />
+                    <Image width={200} src={window.location.href + 'public/img/error.png'} />
                 </div>
             </Card>
         </div>
